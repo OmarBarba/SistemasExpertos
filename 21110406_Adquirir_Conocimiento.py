7efd840456af53702bb16df940faf1bb3789fee9
Omar Alejandro Barba Limón <84804799+OmarBarba@users.noreply.github.com>
2024-02-24 22:11:42 -0600

    conceccion con la base de datos en Acces para ver si funciona correctamente.

--- 21110406_Adquirir_Conocimiento.py
+++ 21110406_Adquirir_Conocimiento.py
@@ -1,3 +1,20 @@
+import pyodbc
+
+# Parámetros de conexión, reemplázalos con los tuyos
+db_path = r'F:\Sistemas_expertos\obtener_conocimiento.accdb'
+connection_str = f"DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={db_path}"
 
 
+try:
+    # Intentar establecer la conexión
+    connection = pyodbc.connect(connection_str)
+    
+    # Si la conexión tiene éxito, imprimir un mensaje de éxito
+    print("¡Conexión exitosa a la base de datos!")
+    
+    # Cerrar la conexión
+    connection.close()
 
+except pyodbc.Error as e:
+    # Si hay un error, imprimir el mensaje de error
+    print(f"Error al conectar a la base de datos: {e}")
