b10c34d4742de78a1cbc27ce043eaa71f09997dd
Omar Alejandro Barba Limón <84804799+OmarBarba@users.noreply.github.com>
2024-02-24 22:22:33 -0600

    se crea la logica con la cual se va a ir mandando la informacion dentro de la base de datos

--- 21110406_Adquirir_Conocimiento.py
+++ 21110406_Adquirir_Conocimiento.py
@@ -4,17 +4,36 @@ import pyodbc
 db_path = r'F:\Sistemas_expertos\obtener_conocimiento.accdb'
 connection_str = f"DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={db_path}"
 
+def conexion():
 
-try:
-    # Intentar establecer la conexión
-    connection = pyodbc.connect(connection_str)
-    
-    # Si la conexión tiene éxito, imprimir un mensaje de éxito
-    print("¡Conexión exitosa a la base de datos!")
-    
-    # Cerrar la conexión
-    connection.close()
+    try:
+        # Intentar establecer la conexión
+        connection = pyodbc.connect(connection_str)
+        
+        # Si la conexión tiene éxito, imprimir un mensaje de éxito
+        print("¡Conexión exitosa a la base de datos!")
+        
+        # Cerrar la conexión
+        connection.close()
 
-except pyodbc.Error as e:
-    # Si hay un error, imprimir el mensaje de error
-    print(f"Error al conectar a la base de datos: {e}")
+    except pyodbc.Error as e:
+        # Si hay un error, imprimir el mensaje de error
+        print(f"Error al conectar a la base de datos: {e}")
+
+
+def chat():
+    print("Hola soy un chatbot que se dedica a obtener informacion y de igual forma a corroborar si ya tengo esa informacion")
+    print("Preguntame lo que sea con confianza: ")
+    
+    #Se asifgna la forma en la que se accede y se manda la pregunta a la base de datos
+    while True:
+        input_usuario = input("Usuario: ")
+        #se crea la forma para poder salir del chatbot
+        if input_usuario.lower() == 'salir':
+            print("Hasta luego. ¡Vuelve pronto!")
+            break
+        #se manda la pregunta a la clase que se dedica a eso.
+        respuesta = obtener_respuesta(input_usuario)
+
+if __name__ == "__main__":
+   chat()
