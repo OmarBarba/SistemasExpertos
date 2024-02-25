2764a6c0f80eb58ffee38292f523c919715753ea
Omar Alejandro Barba Limón <84804799+OmarBarba@users.noreply.github.com>
2024-02-24 22:43:44 -0600

    se hace el esqueleto para el

--- 21110406_Adquirir_Conocimiento.py
+++ 21110406_Adquirir_Conocimiento.py
@@ -9,18 +9,33 @@ def conexion():
     try:
         # Intentar establecer la conexión
         connection = pyodbc.connect(connection_str)
-        
         # Si la conexión tiene éxito, imprimir un mensaje de éxito
-        print("¡Conexión exitosa a la base de datos!")
-        
+        print("¡Conexión exitosa a la base de datos!") 
         # Cerrar la conexión
         connection.close()
-
     except pyodbc.Error as e:
         # Si hay un error, imprimir el mensaje de error
         print(f"Error al conectar a la base de datos: {e}")
 
+def obtener_respuesta(pregunta):
+    connection = pyodbc.connect(connection_str)
+    cursor = connection.cursor()
+    query = "SELECT Respuesta FROM Chat WHERE Pregunta = ?"
+    resultado = cursor.execute(query, pregunta).fetchone()
+    cursor.close()
+    connection.close()
+    return resultado[0] if resultado else None
 
+# Función para almacenar nuevas preguntas y respuestas en la base de datos
+def almacenar_conocimiento(pregunta, respuesta):
+    connection = pyodbc.connect(connection_str)
+    cursor = connection.cursor()
+    query = "INSERT INTO Chat (Pregunta, Respuesta) VALUES (?, ?)"
+    cursor.execute(query, pregunta, respuesta)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
 def chat():
     print("Hola soy un chatbot que se dedica a obtener informacion y de igual forma a corroborar si ya tengo esa informacion")
     print("Preguntame lo que sea con confianza: ")
@@ -34,6 +49,17 @@ def chat():
             break
         #se manda la pregunta a la clase que se dedica a eso.
         respuesta = obtener_respuesta(input_usuario)
+        
+        if respuesta:
+            print(f"Chatbot: {respuesta}")
+        else:
+            print("Chatbot: No tengo una respuesta para eso. ¿Puedes enseñarme algo nuevo?")
+            nuevo_conocimiento = input("Usuario (si/no): ").lower()
+            
+            if nuevo_conocimiento == 'si':
+                respuesta_nueva = input("Usuario: (Ingresa la respuesta que debería tener): ")
+                almacenar_conocimiento(input_usuario, respuesta_nueva)
+                print("Chatbot: ¡Gracias por enseñarme algo nuevo!")
 
 if __name__ == "__main__":
    chat()
